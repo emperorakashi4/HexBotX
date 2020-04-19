1f9aa5589f6073f0552c5dd961fe94c86c279094
emperorakashi4 <63154092+emperorakashi4@users.noreply.github.com>
2020-04-19 23:51:45 +0530

    Add files via upload

---
+++ userbot/plugins/link.py
@@ -0,0 +1,43 @@
+"""Emoji
+
+Available Commands:
+
+.link"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.3
+
+    animation_ttl = range(0, 10)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "link":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+        
+            "[Click Here To Join This Channel](https://t.me/joinchat/AAAAAEeHVdrWMlbMYhVumw)"
+
+ ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 10])
