6010eec26fbbae95cbcb5c75c8117f61c5a1224c
emperorakashi4 <63154092+emperorakashi4@users.noreply.github.com>
2020-04-19 22:21:48 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,7 +11,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in Heroku"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Gracias! HexBot Is Alive!!\n"
+    await alive.edit("`Gracias! HexBot Is Alive!!`\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
                      "`Bot created by:` [Akashi](tg://user?id=1089637689)\n"
                      f"`Forked By`: {DEFAULTUSER}\n"
